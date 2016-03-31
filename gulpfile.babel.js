34b34d060fc241885fd5a42dee2d91e1a8ecf97c
Rasmus <hallstrom.rasmus@gmail.com>
2016-03-31 21:36:35 +0200

    Added sass folder

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,12 +3,22 @@ import run      from 'run-sequence';
 import server   from 'gulp-live-server';
 import rimraf   from 'rimraf';
 import shell    from 'gulp-shell';
+import sass     from 'gulp-sass';
+import sync     from 'browser-sync';
 
 let express;
+
 const paths = {
   js: './src/**/*.js',
+  sass: './src/sass/**/*.scss',
   dest: './app'
 }
+
+gulp.task('browser-sync', function() {
+    sync.init({
+        proxy: "localhost:1337"
+    });
+});
 
 gulp.task('default', ()=> {
   run('server', 'build', 'watch');
@@ -26,6 +36,14 @@ gulp.task('babel', shell.task([
   'babel src --out-dir app'
 ]));
 
+gulp.task('sass', ()=>{
+  return gulp
+    .src(paths.sass)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./public/css'))
+    .pipe(sync.stream());
+});
+
 gulp.task('server', ()=>{
   express = server.new(paths.dest + '/app.js');
 });
@@ -34,6 +52,7 @@ gulp.task('restart', ()=>{
   express.start.bind(express)();
 });
 
-gulp.task('watch', ()=>{
+gulp.task('watch', ['browser-sync'], ()=>{
+  gulp.watch(paths.sass, ['sass']);
   gulp.watch(paths.js, ['build']);
 });
--- src/app.js
+++ src/app.js
@@ -5,17 +5,22 @@ import morgan     from 'morgan';
 const app = express();
 const PORT = 1337;
 
+app.set('views', __dirname + '/../public');
+app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/../public'));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(PORT, ()=>{
-  console.log('Listen on port 3000!')
+  console.log(`Listen on port ${PORT}!`)
 });
 
-app.get('/hello', (req, res)=>{
-  res.send('world');
+app.get('/', (req, res)=>{
+  res.render('index', {
+    title: 'Home',
+    styles: 'css/styles.css'
+  });
 });
 
 app.get('/add/:y/:x', (req, res)=>{
